b89c077786de7167cf0cf3a59728dc6b137e5c4d
Akshaya R <achur0211@gmail.com>
2026-03-08 17:02:22 +0530

    Update __init__.py

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,7 +1,13 @@
-from flask import Blueprint
+from app.api.health import health_bp
+from app.api.employee import employee_bp
+from app.api.department import department_bp
+from app.api.salary import salary_bp
+from app.api.auth import auth_bp
 
-api_bp = Blueprint("api", __name__)
 
-from .employees import employees_bp
-
-api_bp.register_blueprint(employees_bp, url_prefix="/employees")
+def register_blueprints(app):
+    app.register_blueprint(health_bp)
+    app.register_blueprint(auth_bp, url_prefix="/auth")
+    app.register_blueprint(employee_bp, url_prefix="/employees")
+    app.register_blueprint(department_bp, url_prefix="/departments")
+    app.register_blueprint(salary_bp, url_prefix="/salaries")
